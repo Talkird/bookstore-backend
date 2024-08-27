9bce9d264092c947fbcf02929f8135f6c0d5fe5f
Martina2612 <martiabril2612@gmail.com>
2024-08-27 17:23:33 -0300

    Cambio a enum de Role

--- src/main/java/com/bookstore/backend/model/Role.java
+++ src/main/java/com/bookstore/backend/model/Role.java
@@ -1,26 +1,6 @@
 package com.bookstore.backend.model;
 
-import java.util.List;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-
-@Entity
-public class Role {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column
-    private String description;
-
-    @OneToMany(mappedBy = "role")
-    private List<User> users;
-
-    // @OneToOne
-    // private User user;
+public enum Role{
+    USER,
+    ADMIN
 }
