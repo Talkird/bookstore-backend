0cb3e75934937550f592a4c4dc0cf79b50f75f1c
Juan Andrés Losauro <87399338+Talkird@users.noreply.github.com>
2024-08-27 17:11:45 -0300

    genre controller, repo and service removed

--- src/main/java/com/bookstore/backend/model/Role.java
+++ src/main/java/com/bookstore/backend/model/Role.java
@@ -8,8 +8,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToOne;
-
 
 @Entity
 public class Role {
