f7e39af6377036864bb920c1d65b6d93aaf0217a
Martina2612 <martiabril2612@gmail.com>
2024-08-26 23:18:47 -0300

    Entidad Rol de Usuario

---
+++ src/main/java/com/bookstore/backend/model/Role.java
@@ -0,0 +1,28 @@
+package com.bookstore.backend.model;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
+
+
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String description;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+
+    // @OneToOne
+    // private User user;
+}
