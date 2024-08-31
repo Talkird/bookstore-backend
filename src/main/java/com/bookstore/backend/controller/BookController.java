859e008b4fc4c0bb76b773db5172d055b7298640
Martina2612 <martiabril2612@gmail.com>
2024-08-31 01:02:22 -0300

    nuevas exceptions y modificaciones

--- src/main/java/com/bookstore/backend/controller/BookController.java
+++ src/main/java/com/bookstore/backend/controller/BookController.java
@@ -2,16 +2,23 @@ package com.bookstore.backend.controller;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import com.bookstore.backend.service.BookService;
+import com.bookstore.backend.exceptions.BookAlreadyExistsException;
+import com.bookstore.backend.exceptions.BookNotFoundException;
+import com.bookstore.backend.exceptions.InvalidBookDataException;
 import com.bookstore.backend.model.Book;
 
 @RestController
@@ -39,6 +46,7 @@ public class BookController {
 
     @PutMapping("/books/{id}")
     public Book updateBook(@PathVariable Long id, @RequestBody Book book) {
+        book.setId(id);
         return bookService.updateBook(book);
     }
 
@@ -46,4 +54,5 @@ public class BookController {
     public void deleteBook(@PathVariable Long id) {
         bookService.deleteBook(id);
     }
+
 }
