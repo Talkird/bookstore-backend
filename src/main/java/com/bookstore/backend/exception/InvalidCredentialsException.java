bf8a3de4cdbf5c450aefa4c0dca300c11cc41c77
Juan Andrés Losauro <87399338+Talkird@users.noreply.github.com>
2024-08-30 21:40:11 -0300

    Revert "auth exception + exception controller"

--- src/main/java/com/bookstore/backend/exception/InvalidCredentialsException.java
+++
@@ -1,7 +0,0 @@
-package com.bookstore.backend.exception;
-
-public class InvalidCredentialsException extends RuntimeException {
-    public InvalidCredentialsException(String message) {
-        super(message);
-    }
-}
