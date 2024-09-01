c4f4f2e91a850dbe3ca7b4dd9a690f7f7ea9250e
TavishX <tomaspena@outlook.com>
2024-09-01 15:59:51 -0300

    fixes + checkout in cartController

--- src/main/java/com/bookstore/backend/service/BookService.java
+++ src/main/java/com/bookstore/backend/service/BookService.java
@@ -2,9 +2,6 @@ package com.bookstore.backend.service;
 
 import java.util.List;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-
 import com.bookstore.backend.exceptions.BookAlreadyExistsException;
 import com.bookstore.backend.exceptions.BookNotFoundException;
 import com.bookstore.backend.exceptions.InvalidBookDataException;
@@ -13,7 +10,7 @@ import com.bookstore.backend.model.Genre;
 
 public interface BookService {
 
-    public Page<Book> getBooks(PageRequest pageRequest) throws BookNotFoundException;
+    public List<Book> getBooks() throws BookNotFoundException;
 
     public Book createBook(Book book) throws BookAlreadyExistsException, InvalidBookDataException;
 
