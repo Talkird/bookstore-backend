8c091df1980df563bb057ab1abe03b58d9c20cea
Santiago6546 <smociulsky@uade.edu.ar>
2024-09-08 17:45:42 -0300

    exceptions de order y rating + updateUser

--- src/main/java/com/bookstore/backend/service/order/OrderService.java
+++ src/main/java/com/bookstore/backend/service/order/OrderService.java
@@ -2,11 +2,14 @@ package com.bookstore.backend.service.order;
 
 import java.util.List;
 
+import com.bookstore.backend.exception.auth.UserNotFoundException;
+import com.bookstore.backend.exception.order.OrderNotFoundException;
 import com.bookstore.backend.model.order.Order;
 
 public interface OrderService {
-    // get userId from Order object cart
-    public List<Order> getOrdersByUserId(Long userId); // TODO agregar throws exceptions
+    public List<Order> getOrdersByUserId(Long userId) throws UserNotFoundException;
 
     public Order createOrder(Order order);
+
+    public Order getOrderById(Long orderId) throws OrderNotFoundException;
 }
