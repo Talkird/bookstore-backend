81f7d818b4e5ae3901c3d5d633bbd20839c9f5b2
fedemuntaabski <fmuntaabski@uade.edu.ar>
2024-09-08 16:44:46 -0300

    securityconfig books

--- src/main/java/com/bookstore/backend/jwt/config/SecurityConfig.java
+++ src/main/java/com/bookstore/backend/jwt/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.bookstore.backend.jwt.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,6 +10,8 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.bookstore.backend.model.user.Role;
 
 import lombok.RequiredArgsConstructor;
 
@@ -27,6 +30,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests(req -> req
                         // Endpoints públicos y de error
                         .requestMatchers("/api/v1/auth/**").permitAll() // Registro, login, etc.
+                        .requestMatchers(HttpMethod.GET,"/books/all").hasAnyAuthority(Role.USER.name())
+                        .requestMatchers(HttpMethod.GET,"/books/{id}").hasAnyAuthority(Role.USER.name())
+                        .requestMatchers("/books/**").hasAnyAuthority(Role.ADMIN.name())
                         .anyRequest()
                         .authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
