c6a90a75ae2b20dea3778e9ecb1b0cb709d32675
Juan Andrés Losauro <losaurojuan@gmail.com>
2024-09-10 12:43:16 -0300

    pull main, resolved conflicts

--- src/main/java/com/bookstore/backend/controller/CartController.java
+++ src/main/java/com/bookstore/backend/controller/CartController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bookstore.backend.model.dto.CartItemRequest;
@@ -19,7 +18,6 @@ import com.bookstore.backend.model.dto.OrderRequest;
 import com.bookstore.backend.service.cart.CartService;
 
 @RestController
-@RequestMapping("/carts")
 public class CartController {
 
     @Autowired
